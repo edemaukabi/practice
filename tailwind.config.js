a0190cc98ac684aa3a1966d7f5c9a866281e2d82
unknown <edemaukabi@gmail.com>
2024-03-11 19:56:05 +0100

    add files

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontSize: {
+        "big": "44px",
+        "small": "14px"
+      },
+    },
+  },
+  plugins: [],
+}
+
